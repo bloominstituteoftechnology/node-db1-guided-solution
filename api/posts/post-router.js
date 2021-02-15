4e248901ec206b3dbca166ebb3896e64a28a8987
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-02-15 15:54:41 +0100

    comments

--- api/posts/post-router.js
+++ api/posts/post-router.js
@@ -3,20 +3,46 @@ const Post = require('./post-model');
 
 const router = express.Router();
 
+// MIDDLEWARES, ELSE THE LOGIC GETS VERY REPETITIVE IN THE ENDPOINTS!!
+// these could live in a separate module
+async function checkId(req, res, next) {
+  try {
+    const post = await Post.getById(req.params.id)
+    if (post) {
+      req.post = post; // used in the [GET] api/posts/:id endpoint
+      next();
+    } else {
+      res.status(404).json({ message: 'Post not found' });
+    }
+  } catch (err) { next(err) }
+}
+
+function checkPayload(req, res, next) {
+  const { title, contents } = req.body
+  if (title && contents) {
+    req.post = { title, contents } // used in the [POST] api/posts endpoint
+    next();
+  } else {
+    res.status(400).json({ message: 'title and contents are required' });
+  }
+}
+
+// ENDPOINTS
 router.get('/', async (req, res, next) => {
   try {
     const posts = await Post.get();
     res.json(posts);
-  } catch (err) { next(err); }
+  } catch (err) { next(err); } // use next(err) to avoid repeating the same error logic
 });
 
 router.get('/:id', checkId, async (req, res) => {
+  // see how skinny the endpoint becomes thanks to middleware?
   res.status(200).json(req.post);
 });
 
 router.post('/', checkPayload, async (req, res, next) => {
   try {
-    const newPost = await Post.create(req.post)
+    const newPost = await Post.create(req.post) // req.post is sure to have the correct shape
     res.status(201).json(newPost);
   } catch (err) { next(err); }
 });
@@ -39,31 +65,6 @@ router.delete('/:id', checkId, async (req, res, next) => {
 // AND IT NEEDS THE `next` PARAMETER EVEN IF IT'S NOT USED!!
 router.use((err, req, res, next) => {
   res.status(500).json({ message: err.message, stack: err.stack });
-})
-
-// MIDDLEWARES ELSE THE LOGIC GETS VERY REPETITIVE IN THE ENDPOINTS!!
-// MIDDLEWARES ELSE THE LOGIC GETS VERY REPETITIVE IN THE ENDPOINTS!!
-// MIDDLEWARES ELSE THE LOGIC GETS VERY REPETITIVE IN THE ENDPOINTS!!
-async function checkId(req, res, next) {
-  try {
-    const post = await Post.getById(req.params.id)
-    if (post) {
-      req.post = post; // used in the getById endpoint
-      next();
-    } else {
-      res.status(404).json({ message: 'Post not found' });
-    }
-  } catch (err) { next(err) }
-}
-
-function checkPayload(req, res, next) {
-  const { title, contents } = req.body
-  if (title && contents) {
-    req.post = { title, contents } // not used but nice to have
-    next();
-  } else {
-    res.status(400).json({ message: 'title and contents are required' });
-  }
-}
+});
 
 module.exports = router;
